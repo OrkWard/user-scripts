1bb3746b59fb726a4ba8001411e25915c3e6ff9f
OrkWard <orkward_o@outlook.com>
2023-12-23 02:40:00 +0800

    feat(github-mark-read): trigger mark on location change

--- mark-github-issues-as-read.user.js
+++ mark-github-issues-as-read.user.js
@@ -5,132 +5,91 @@
 // @description  mark issues as read
 // @author       OrkWard
 // @require      https://code.jquery.com/jquery-2.1.4.min.js
-// @match        *//github.com/*/*/issues*
-// ==UserScript==
-// @name         mark issues as read
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  mark issues as read
-// @author       OrkWard
-// @require      https://code.jquery.com/jquery-2.1.4.min.js
-// @match        *//github.com/*/*/issues*
-// @match        https://github.com/*/*/issues*
+// @match        https://github.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
 // @grant        none
+// @run-at document-idle
 // ==/UserScript==
 
+(() => {
+  let oldPushState = history.pushState;
+  history.pushState = function pushState() {
+    let ret = oldPushState.apply(this, arguments);
+    window.dispatchEvent(new Event('pushstate'));
+    window.dispatchEvent(new Event('locationchange'));
+    return ret;
+  };
+
+  let oldReplaceState = history.replaceState;
+  history.replaceState = function replaceState() {
+    let ret = oldReplaceState.apply(this, arguments);
+    window.dispatchEvent(new Event('replacestate'));
+    window.dispatchEvent(new Event('locationchange'));
+    return ret;
+  };
+
+  window.addEventListener('popstate', () => {
+    window.dispatchEvent(new Event('locationchange'));
+  });
+})();
 
 (function() {
   'use strict';
 
-
-
-
-  if (localStorage.visitedList === undefined || Array.isArray(JSON.parse(localStorage.visitedList))) {
-    localStorage.setItem('visitedList', JSON.stringify({}));
-  }
-  if (Object.keys(localStorage.visitedList).length > 50000) {
-    localStorage.setItem('visitedList', JSON.stringify({}));
-  }
-
-
-   issues content page
-  if (document.location.pathname.match(/issues\/\d.*/)) {
-    const visitedList = JSON.parse(localStorage.visitedList);
-    const path = document.location.pathname;
-    const key = path.slice(0, path.indexOf('issues'));
-    visitedList[key] = visitedList[key] ?? [];
-    visitedList[key].push('issue_' + path.slice(path.indexOf('issues') + 7));
-    localStorage.setItem('visitedList', JSON.stringify(visitedList));
-  }
-
-
-  addEventListener('storage', (event) => {
-    weakenVisited();
-  })// ==UserScript==
-// @name         mark issues as read
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  mark issues as read
-// @author       OrkWard
-// @require      https://code.jquery.com/jquery-2.1.4.min.js
-// @match        *//github.com/*/*/issues*
-// @match     
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
-// @grant        none
-// ==/UserScript==
-
-
-(function() {
-  'use strict';
-
-
-
-
-  if (localStorage.visitedList === undefined || Array.isArray(JSON.parse(localStorage.visitedList))) {
-    localStorage.setItem('visitedList', JSON.stringify({}));
-  }
-  if (Object.keys(localStorage.visitedList).length > 50000) {
-    localStorage.setItem('visitedList', JSON.stringify({}));
-  }
-
-
-   issues content page
-  if (document.location.pathname.match(/issues\/\d.*/)) {
-    const visitedList = JSON.parse(localStorage.visitedList);
-    const path = document.location.pathname;
-    const key = path.slice(0, path.indexOf('issues'));
-    visitedList[key] = visitedList[key] ?? [];
-    visitedList[key].push('issue_' + path.slice(path.indexOf('issues') + 7));
-    localStorage.setItem('visitedList', JSON.stringify(visitedList));
-  }
-
-
-  addEventListener('storage', (event) => {
-    weakenVisited();
-  })
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
-// @grant        none
-// ==/UserScript==
-
-(function() {
-  'use strict';
-
-
-  if (localStorage.visitedList === undefined || Array.isArray(JSON.parse(localStorage.visitedList))) {
-    localStorage.setItem('visitedList', JSON.stringify({}));
-  }
-  if (Object.keys(localStorage.visitedList).length > 50000) {
+  // clear & reset
+  try {
+    if (localStorage.visitedList === undefined || typeof JSON.parse(localStorage.visitedList) !== 'object') {
+      localStorage.setItem('visitedList', JSON.stringify({}));
+    }
+    if (Object.keys(localStorage.visitedList).length > 50000) {
+      localStorage.setItem('visitedList', JSON.stringify({}));
+    }
+  } catch(err) {
     localStorage.setItem('visitedList', JSON.stringify({}));
   }
 
   // issues content page
-  if (document.location.pathname.match(/issues\/\d.*/)) {
-    const visitedList = JSON.parse(localStorage.visitedList);
-    const path = document.location.pathname;
-    const key = path.slice(0, path.indexOf('issues'));
-    visitedList[key] = visitedList[key] ?? [];
-    visitedList[key].push('issue_' + path.slice(path.indexOf('issues') + 7));
-    localStorage.setItem('visitedList', JSON.stringify(visitedList));
+  function addItem () {
+    if (document.location.pathname.match(/issues\/\d.*/)) {
+      console.log('add item');
+      const visitedList = JSON.parse(localStorage.visitedList);
+      const path = document.location.pathname;
+      const key = path.slice(0, path.indexOf('issues'));
+      visitedList[key] = visitedList[key] ?? [];
+      visitedList[key].push('issue_' + path.slice(path.indexOf('issues') + 7));
+      localStorage.setItem('visitedList', JSON.stringify(visitedList));
+    }
+
   }
 
-  addEventListener('storage', (event) => {
-    weakenVisited();
-  })
 
   // turn the visited post to gray
   function weakenVisited() {
+    console.log('weaken');
     const visitedList = JSON.parse(localStorage.visitedList);
     const path = document.location.pathname;
     const key = path.slice(0, path.indexOf('issues'));
     const thisRepoVisited = visitedList[key];
     if (thisRepoVisited) {
       thisRepoVisited.forEach((issues) => {
-        console.log('#' + issues + ' a');
+        console.log(`#${issues}`);
         $('#' + issues + ' a').attr('style', 'color: #CCCCCC !important');
       });
     }
   }
 
+  addItem();
   weakenVisited();
+
+  // github use client-side router
+  addEventListener('storage', (event) => {
+    weakenVisited();
+  })
+  addEventListener('locationchange', (event) => {
+    setTimeout(() => {
+      console.log('locationchange');
+      weakenVisited();
+      addItem();
+    }, 500);
+  });
 })();
